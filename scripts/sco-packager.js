fa97824fdbd9590561d83e87412a449dcb9adfad
Jhony Moncada <2750497+jhonymoncada@users.noreply.github.com>
2023-09-29 17:01:12 -0500

    Scorm updates.

--- scripts/sco-packager.js
+++ scripts/sco-packager.js
@@ -3,8 +3,8 @@ var path = require('path');
 
 const config = {
   version: '1.2',
-  organization: 'My Amazing Company',
-  title: 'My Course',
+  organization: 'Civil Aviation | CAE',
+  title: 'AW139',
   language: 'en-US',
   masteryScore: 80,
   startingPage: 'index.html',
@@ -12,19 +12,19 @@ const config = {
   package: {
       version: process.env.npm_package_version,
       zip: true,
-      author: 'Firstname Lastname',
+      author: 'Xennial Digital',
       outputFolder: path.join(__dirname, '../scorm-packages'),
-      description: 'Here goes the description for the scorm package',
-      keywords: ['scorm', 'test', 'course'],
+      description: 'AW139 Simulator',
+      keywords: ['scorm', 'aw139', 'course'],
       typicalDuration: 'PT0H5M0S',
-      rights: `©${new Date().getFullYear()} My Amazing Company. All rights reserved.`,
+      rights: `©CAE Inc. ${new Date().getFullYear()}. All rights reserved.`,
       vcard: {
-        author: 'Firstname Lastname',
-        org: 'My Amazing Company',
-        tel: '(000) 000-0000',
-        address: '123 Sample street. Tampa, FL 33626',
-        mail: 'my@email.com',
-        url: 'https://www.amazingcompany.com'
+        author: 'Jhony Moncada',
+        org: 'Xennial Digital',
+        tel: '(754) 273-7633',
+        address: '2200 Biscayne Blvd, Miami, FL 33137',
+        mail: 'xennial@xennialdigital.com',
+        url: 'https://xennialdigital.com/'
       }
   }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
 import React from 'react';
 import './styles/global.scss'
-import { HashRouter as Router, Route, Link, Routes } from "react-router-dom";
-import DefaultLayout from './components/Layout';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './pages/Home';
 import MaintenanceMessagesPage from './pages/MaintenanceMessages';
 import ActivePage from './pages/maintenance-messages/Active';
-
-const Navigation = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Page 1</Link>
-        </li>
-        <li>
-          <Link to="/maintenance-messages">Maintenance</Link>
-        </li>
-        <li>
-          <Link to="/page-2">Page 2</Link>
-        </li>
-        <li>
-          <Link to="/page-3">Page 3</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-const Page2 = () => {
-  return (
-    <DefaultLayout>
-      Page 2
-      <Navigation />
-    </DefaultLayout>
-  );
-};
-
-
-const Page3 = () => {
-  return (
-    <div>
-      Page 3
-      <Navigation />
-    </div>
-  );
-};
 
 function App() {
   return (
@@ -53,8 +12,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/maintenance-messages" element={<MaintenanceMessagesPage />} />
         <Route path="/maintenance-messages/active" element={<ActivePage />} />
-        <Route path="/page-2" element={<Page2 />} />
-        <Route path="/page-3" element={<Page3 />} />
       </Routes>
     </Router>
   );
