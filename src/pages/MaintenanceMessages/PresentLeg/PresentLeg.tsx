8e6af305d41e0af837d89b959169577b60a0a1ea
Camilo Galvis <camilo@dsinno.io>
2023-11-20 18:53:29 -0500

    [G500600-4] Adding subitems for 23,24,25,27 and items 75-78,80 into Present Leg

--- src/pages/MaintenanceMessages/PresentLeg/PresentLeg.tsx
+++ src/pages/MaintenanceMessages/PresentLeg/PresentLeg.tsx
@@ -21,10 +21,41 @@ const PresentLeg = () => {
         { text: '30 AUTO THROTTLE' },
       ],
     },
-    { text: '23 Communications' },
-    { text: '24 Electrical Power' },
-    { text: '25 EQUIPMENT/FURNISHINGS' },
-    { text: '27 Flight Controls' },
+    { 
+      text: '23 Communications',
+      children: [
+        { text: '00 COMMUNICATIONS'},
+        { text: '10 SPEECH COMMUNICATIONS'},
+        { text: '20 DATA TRANSMISSION AND AUTO-CALLING'},
+        { text: '50 AUDIO INTEGRATING'},
+        { text: '70 AUDIO AND VIDEO MONITORING'}
+      ]
+    },
+    { 
+      text: '24 Electrical Power',
+      children: [
+        { text: '20 AC GENERATION' },
+        { text: '80 ELECTRICAL POWER MULTIPLEXING' }
+      ] 
+    },
+    { 
+      text: '25 EQUIPMENT/FURNISHINGS',
+      children: [
+        { text: '10 FLIGHT COMPARTMENT'}
+      ] 
+    },
+    { 
+      text: '27 Flight Controls',
+      children: [
+        { text: '00 FLIGHT CONTROLS' },
+        { text: '10 ALERON CONTROL SYSTEM' },
+        { text: '20 RUDDER CONTROL SYSTEM' },
+        { text: '30 ELEVATOR CONTROL SYSTEM' },
+        { text: '50 FLAP CONTROL SYSTEM' },
+        { text: '60 SPOILER CONTROL SYSTEM' }
+      ]
+    },
+    { text: '28 Fuel' },
     { text: '30 Ice And Rain Protection' },
     { text: '31 Indicating/Recording Systems' },
     { text: '32 Landing Gear' },
@@ -39,7 +70,12 @@ const PresentLeg = () => {
     { text: '49 Airborne Auxiliary Power' },
     { text: '52 Doors' },
     { text: '73 Engine Fuel And Control' },
+    { text: '75 Air' },
+    { text: '76 Engine Controls' },
+    { text: '77 Engine Indicating' },
+    { text: '78 Exhaust' },
     { text: '79 Oil' },
+    { text: '80 Starting' },
   ]
 
   const navBarButton: NavBarButtonType = {
