9bb31349e4dee2a8876bb286610cb4484f51380c
Camilo Galvis <camilo@dsinno.io>
2023-11-29 19:18:05 -0500

    [G500600-35] Correcting redirection test menu for 33 concentrators 12 & 13

--- src/pages/SystemDiagnostics/FortyTwoIntegratedModularAvionics/ThirtyThreeDCNRemoteDataConcentratorThirteen/DataDCNRDCThirteenStatus/DataDCNRDCThirteenStatusTwo.tsx
+++ src/pages/SystemDiagnostics/FortyTwoIntegratedModularAvionics/ThirtyThreeDCNRemoteDataConcentratorThirteen/DataDCNRDCThirteenStatus/DataDCNRDCThirteenStatusTwo.tsx
@@ -30,7 +30,7 @@ const DataDCNRDCThirteenStatusTwo = () => {
       disabled: false,
       position: 1,
       onClick: () => { 
-        navigate(-1)
+        navigate('/systemdiagnostics')
       },
     },
     {
